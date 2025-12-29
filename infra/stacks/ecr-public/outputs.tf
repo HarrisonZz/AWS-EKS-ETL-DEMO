81b38a572560f34ccaf5dc556c1e87f6373b4655
Harrison <harrison_0418@outlook.com>
2025-12-30 02:08:58 +0800

    Move EKS stack path

--- infra/stacks/ecr-public/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "etl_demo_ecr_public_repo_uri" {
-  value = module.etl_demo_ecr_public_repo_uri.repository_uri
-}
--- infra/stacks/ecr-public/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "repository_name" {
-  type    = string
-  default = "etl-demo"
-}
-
-variable "description" {
-  type    = string
-  default = "ETL related image for demo"
-}
